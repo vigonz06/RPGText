fb8057c18e91cdd038892fa7513b6308181cbc33
sarfios20 <david-hpf@hotmail.com>
2015-12-26 21:19:27 +0100

    struct Objeto

--- Player.h
+++ Player.h
@@ -4,9 +4,19 @@
 #include "Characters.h"
 #include "Arma.h"
 
+struct Objeto
+{
+	std::string id;
+	std::string tipo;
+	std::string descripcion;
+	int num_objetos//limitar un maximo
+
+};
+
 struct Player : public Character
 {
 	Arma arma_equipada;
+	//inventario
 
 };
 
