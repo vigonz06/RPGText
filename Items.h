4b7682806fbed7b48fba45b48bb0f3bb8a1ffc3b
vigonz06 <victorglez96@gmail.com>
2015-12-26 10:45:39 +0100

    workin' on Items

--- Items.h
+++ Items.h
@@ -2,14 +2,58 @@
 #define ITEMS
 
 //to do David
-//pon ahi las caracteristicas que quieras de los items 
-//(nombre, descripcion...) no olvides meter una booleana 
-//que indique si se ha encontrado. implementa tambien 
-//una funcion de guardar y otra de cargar dicho item
+//le pongo esto de momento, si se te ocurren mas cosas,
+//o ves que sobrea el propierties, editalo, pero no olvides
+//adaptar la funcion de carga
+
+#include <string>
+#include <fstream>
 
 struct Item
 {
+	std::string name;
+	bool found;
+	std::string properties;
 
+	bool load(std::ifstream &file);
+	void find();
 };
 
-#endif
+bool Item::load(std::ifstream &file)
+{
+	std::string line, text = "";
+
+	std::getline(file, name);
+
+	if (!file.fail())
+	{
+		file >> found;
+
+		if (!file.fail())
+		{
+			do
+			{
+				std::getline(file, line);
+				text += line + '\n';
+			} while (line != "" && !file.fail());
+
+			if (!file.fail())
+			{
+				properties = text;
+
+				return true;
+			}
+			else return false;
+		}
+		else return false;
+	}
+	else return false;
+}
+
+void Item::find()
+{
+	if (!found)
+	{
+		found = true;
+	}
+}#endif
