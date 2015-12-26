45c76191e3d27ad084d5b55f82352bbb5bf6752f
sarfios20 <david-hpf@hotmail.com>
2015-12-26 17:35:19 +0100

    struct character y ataque

--- Characters.h
+++ Characters.h
@@ -3,29 +3,27 @@
 
 #include <string>
 
-//Si vas a hacer una estructura de ataque, con daño, municion, mana, defensa, magie, etc...
+//Si vas a hacer una estructura de ataque, con daÃ±o, municion, mana, defensa, magie, etc...
 //supongo que sera comun tanto para el personaje como para los npc, por tanto ponla aqui, 
 //encima de la struct characters
 
-/*struct Ataque
+struct Ataque
 {
-	//por ejemplo
-};*/
+ 	int danio;
+	int mana;
+	int municion;
+
+};
 
 struct Character
 {
 	std::string id; //evidente que ambos van a tener nombres
 
-	//to do David
-	//caracteristicas comunes 
-	//tanto de los npc como del personaje
-	//salud, daño, mana, etc (lo que quieras que tengan,
-	//pero recuerda, solo lo que sea comun a ambos)
-
-	//para que entiendas la herencia:
-	//ahora no hace falta declarar la variable id ni en
-	//personaje ni en npc, ya que al ser esta una structura 
-	//base de ambos, dicha variable pasa a ser de ellos
+	string nombre;
+	int vida;
+	int mana;
+	int municion;
+	int armadura;
 };
 
 #endif
