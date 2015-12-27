b4243ccb2073b0576bbb6cc2e2772a3c07012eb7
vigonz06 <victorglez96@gmail.com>
2015-12-27 20:10:45 +0100

    no save&load

--- Clase.h
+++ Clase.h
@@ -8,39 +8,6 @@ struct Clase
 {
 	std::string id;
 	std::string descripcion;
-
-	bool load(std::ifstream &file);
-	void save(std::ofstream &file);
 };
-
-bool Clase::load(std::ifstream &file)
-{
-	std::string line, text = "";
-
-	file >> id;
-
-	if (!file.fail())
-	{
-		do
-		{
-			std::getline(file, line);
-			text += line + '\n';
-		} while (line != "" && !file.fail());
-
-		descripcion = text;
-
-		if (!file.fail()) return true;
-
-		else return false;
-	}
-	else return false;
-}
-
-void Clase::save(std::ofstream &file)
-{
-	file << id << std::endl
-		<< descripcion << std::endl
-		<< std::endl;
-}
 
 #endif
