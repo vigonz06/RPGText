fb28136a529d598f4a1152b41b57a9769daea40e
vigonz06 <victorglez96@gmail.com>
2015-12-27 20:03:57 +0100

    ropas
    incompleto (como todo)

--- Player.h
+++ Player.h
@@ -6,11 +6,24 @@
 #include "ItemList.h"
 #include "Characters.h"
 
+typedef enum Prendas { Casco, Pechera, Capa, Pantalones, Botas }
+
+//to do Victor
+//lista de tipos
+//compartimentar
+ struct Ropa
+ {
+ 	Prendas prenda;
+ 	std::string id;
+ 	std::string descripcion;
+ 	int mejora;
+ }
 
 struct Player : public Character
 {
 	Arma arma_equipada;
 	ItemList inventario;
+	Ropa ropas;
 	int mapa;
 
 	bool load();
