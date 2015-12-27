a97a0f565f1b33d32592cbcc4509a103b2681e7d
vigonz06 <victorglez96@gmail.com>
2015-12-27 20:10:01 +0100

    no save&load

--- Arma.h
+++ Arma.h
@@ -10,51 +10,6 @@ struct Arma
 	int municion;
 	int mod_daño;
 	std::string descripcion;
-
-	bool load(std::ifstream &file);
-	void save(std::ofstream &file);
 };
-
-bool Arma::load(std::ifstream &file)
-{
-	std::string line, text = "";
-
-	file >> id;
-
-	if (!file.fail())
-	{
-		file >> municion;
-
-		if (!file.fail())
-		{
-			file >> mod_daño;
-
-			if (!file.fail())
-			{
-				do
-				{
-					std::getline(file, line);
-					text += line + '\n';
-				} while (line != "" && !file.fail());
-
-				descripcion = text;
-
-				if (!file.fail()) return true;
-
-				else return false;
-			}
-			else return false;
-		}
-		else return false;
-	}
-	else return false;
-}
-
-void Arma::save(std::ofstream &file)
-{
-	file << id << " " << mod_daño
-		<< std::endl << descripcion
-		<< std::endl << std::endl;
-}
 
 #endif
