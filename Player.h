3cbb8a8fa50cf30af73159c725e3165e0ea1820c
vigonz06 <victorglez96@gmail.com>
2015-12-27 20:13:11 +0100

    no save&load

--- Player.h
+++ Player.h
@@ -25,45 +25,6 @@ struct Player : public Character
 	ItemList inventario;
 	Ropa ropas;
 	int mapa;
-
-	bool load();
-	void save();
 };
-
-bool Player::load()
-{
-	std::ifstream file;
-
-	file.open("Player.txt");
-
-	if (file.is_open());
-	{
-		file >> id;
-
-		if (!file.fail())
-		{
-			file >> mapa;
-
-			if (!file.fail() && clase.load(file) && defensa.load(file) && ataque.load(file) && arma_equipada.load(file) && inventario.load(file)) return true;
-
-			else return false;
-		}
-	}
-}
-
-void Player::save()
-{
-	std::ofstream file;
-
-	file.open("Player.txt");
-
-	file << id << " " <<
-		mapa << std::endl;
-
-	defensa.save(file);
-	ataque.save(file);
-	arma_equipada.save(file);
-	inventario.save(file);
-}
 
 #endif
