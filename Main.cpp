c39a2bfd3987b948faf246e0d837158b9ed95035
vigonz06 <victorglez96@gmail.com>
2016-01-16 21:11:50 +0100

    bienvenida added

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,24 @@
 #include "PlaceList.h"
 #include "Player.h"
 
+
+#include <iostream>
+#include "Archivo.h"
+
+void bienvenida(Player * player)
+{
+    //rellena los campos player id y clase id
+    //todo rellenar mas campos player y clase
+    std::cout << find("id_bienvenida_nombre") << std::endl;
+    std::cin >>  player->id;
+    std::cout << "\n" <<find("id_bienvenida_clase_1") << player->id << find("id_bienvenida_clase_2") << std::endl;
+    std::cin >>  player->clase.id;
+    std::cout << find("id_bienvenida_final") << std::endl;
+    std::string centinela_clase= "clase_" + player->clase.id;
+    player->clase.descripcion = find(centinela_clase);
+
+}
+
 void main()
 {
 	
